a48a8e0a4069db98e67e0fff9ebaa362f92e0005
Simone Saleri <65787490+simonesaleri@users.noreply.github.com>
2021-04-17 14:42:32 +0200

    Create MainView_builder.java
    main view for software

---
+++ Helios/src/helios/core/MainView_builder.java
@@ -0,0 +1,43 @@
+package helios.core;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class MainView_builder {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainView_builder window = new MainView_builder();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainView_builder() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
